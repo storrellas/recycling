199d111148338baa8cb4c48bfc06b533f2949787
storrella <storrellas@gmail.com>
2019-01-13 23:24:55 +0100

    ENH: Generating endpoint

--- api/views.py
+++ api/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Dependencies
 from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
 
 
 # Project
@@ -40,3 +44,28 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     renderer_classes = (JSONRenderer, )
+
+    @action(detail=True, methods=['get'])
+    def locatespot(self, request, pk=None):
+
+        # Retrieve object
+        product = self.get_object()
+
+        # Component set
+        component_set = product.component_set.all()
+
+        # Get Recyclable Spot
+        recyclable_spot_list = []
+        for component in component_set:
+            spot_queryset = RecyclableSpot.objects.filter(recyclable_component=component.recyclable_component)
+            spot_list = list(spot_queryset.values_list('id', flat=True))
+            recyclable_spot_list.extend(spot_list)
+
+        print("-- Recyclable Spot List--")
+        print(recyclable_spot_list)
+
+        # Get RecyclableSpot List
+        recyclable_spot_queryset = RecyclableSpot.objects.filter(pk__in=recyclable_spot_list)
+
+        serializer = RecyclableSpotSerializer(recyclable_spot_queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
