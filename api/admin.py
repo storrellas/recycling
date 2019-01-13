7f9c243ae05908f1036288f6b4d476051f8eeac5
storrella <storrellas@gmail.com>
2019-01-13 12:24:07 +0100

    ENH: Generated models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+from django.apps import apps
 
-# Register your models here.
+
+app_models = apps.get_app_config('api').get_models()
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
--- api/models.py
+++ api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class RecyclableProduct(models.Model):
+    name = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    barcode = models.CharField(max_length=500, null=True)
+    name = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
+
+class RecyclableSpot(models.Model):
+    name = models.CharField(max_length=500, null=True)
+    address = models.CharField(max_length=500, null=True)
+    latitude = models.DecimalField(max_digits=5, null=True, decimal_places=2)
+    longitude = models.DecimalField(max_digits=5, null=True, decimal_places=2)
+    recyclable_product = models.ForeignKey(RecyclableProduct, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Components(models.Model):
+    name = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True)
+    product_set = models.ManyToManyField(Product, related_name="components_set", blank=True)
+    recyclable_product = models.ForeignKey(RecyclableProduct, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
