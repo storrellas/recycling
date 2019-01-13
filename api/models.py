54b0a8c05bdbc1293ca8099d8e25046237b640ea
storrella <storrellas@gmail.com>
2019-01-13 22:24:43 +0100

    ENH: Generating fixtures

--- api/models.py
+++ api/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
-class RecyclableProduct(models.Model):
-    name = models.CharField(max_length=500, null=True)
-    description = models.CharField(max_length=500, null=True)
-
-    def __str__(self):
-        return self.name
-
-class Product(models.Model):
-    barcode = models.CharField(max_length=500, null=True)
+class RecyclableComponent(models.Model):
     name = models.CharField(max_length=500, null=True)
     description = models.CharField(max_length=500, null=True)
 
@@ -20,7 +12,7 @@ class RecyclableSpot(models.Model):
     address = models.CharField(max_length=500, null=True)
     latitude = models.DecimalField(max_digits=5, null=True, decimal_places=2)
     longitude = models.DecimalField(max_digits=5, null=True, decimal_places=2)
-    recyclable_product = models.ForeignKey(RecyclableProduct, on_delete=models.SET_NULL, null=True)
+    recyclable_component = models.ForeignKey(RecyclableComponent, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -28,8 +20,16 @@ class RecyclableSpot(models.Model):
 class Component(models.Model):
     name = models.CharField(max_length=500, null=True)
     description = models.CharField(max_length=500, null=True)
-    product_set = models.ManyToManyField(Product, related_name="components_set", blank=True)
-    recyclable_product = models.ForeignKey(RecyclableProduct, on_delete=models.SET_NULL, null=True)
+    recyclable_component = models.ForeignKey(RecyclableComponent, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    barcode = models.CharField(max_length=500, null=True)
+    name = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True)
+    component_set = models.ManyToManyField(Component, related_name="product_set", blank=True)
 
     def __str__(self):
         return self.name
