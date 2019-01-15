6690aabe6d6717b9961ac605b2f23ffdd6a51a72
storrellas <storrellas@gmail.com>
2019-01-15 08:48:42 +0000

    ENH: ALLOWED_HOSTS

--- recycling/settings.py
+++ recycling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!%kaycpb#yas_z*=^zt52yxpy3g4+_*hgd+*a0%^l6iut6z#o)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
